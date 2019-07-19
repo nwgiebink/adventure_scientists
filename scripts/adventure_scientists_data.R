c93f8456f5a40b37918d2a6858077878db85235c
Noah Giebink <nwgiebink@gmail.com>
2019-07-19 14:37:54 -0700

    script for adventure scientists occurence data

---
+++ scripts/adventure_scientists_data.R
@@ -0,0 +1,8 @@
+#Occurence data from Adventure Scientists
+#Noah Giebink
+#giebink@email.arizona.edu
+#2019-07-19
+
+#data
+adventure_scientists_data_raw <- 
+  read.csv("data/iNaturalist_AdventureScientists.csv")
