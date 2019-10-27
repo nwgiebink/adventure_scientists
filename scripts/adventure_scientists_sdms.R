fdee8265b85e2e897603361ba3eadca0584317d2
Noah Giebink <nwgiebink@gmail.com>
2019-10-27 16:05:36 -0700

    Map G. patrobas SDM using all data

--- scripts/adventure_scientists_sdms.R
+++ scripts/adventure_scientists_sdms.R
@@ -8,6 +8,18 @@
 library(tidyverse)
 
 #Data --------------------------------------------------------------------
+  
+# Data from later outputs
+G_patrobas_iNat <- readRDS('data/G_patrobas_iNat.rds')
+G_patrobas_iNat_blocked <- readRDS("data/G_patrobas_iNat_blocked.rds")
+G_patrobas_iNat2 <- read.csv("data/G_patrobas_iNat2.csv")
+G_patrobas_iNat2_split <- readRDS('data/G_patrobas_iNat2_split.rds')
+G_patrobas_iNat_model <- readRDS("data/G_patrobas_inat_model.rds")
+G_patrobas_iNat_best <- readRDS("data/G_patrobas_iNat_best.rds")
+G_patrobas_iNat_eval <- readRDS("data/G_patrobas_iNat_eval.rds")
+G_patrobas_iNat_full <- readRDS("data/G_patrobas_iNat_full.rds")
+
+# Data from fresh start
   #All research-grade AS and iNat butterfly (Papilionoidea) 
   #records within swlat 28.0 swlng -125.0 nelat 49.0 nelng -100.0
 west_as <- read.csv("data/west_as.csv")
@@ -17,30 +29,31 @@ west_iNat <- read.csv("data/west_iNat.csv")
 west_as_observations <- select(west_as, id)
 west_iNat_only <- filter(west_iNat, !(west_iNat$id %in% as_observations$id))
 
-#' iNat data
-#'  iNat + AS = west_iNat
-#'  iNat only = west_iNat_only
-#'  AS only = west_as
-#'  just the candidates:
-
+  # Candidates all data (iNat + AS)
 west_iNat_cand <- filter(west_iNat, scientific_name == "Libytheana carinenta"|
                            scientific_name == "Celastrina echo"|
                            scientific_name == "Gyrocheilus patrobas"|
                            scientific_name == "Eurema mexicana"|
                            scientific_name == "Vanessa cardui")
+  # candidates iNat data only
 west_iNat_only_cand <- filter(west_iNat_only, scientific_name == "Libytheana carinenta"|
                                 scientific_name == "Celastrina echo"|
                                 scientific_name == "Gyrocheilus patrobas"|
                                 scientific_name == "Eurema mexicana"|
                                 scientific_name == "Vanessa cardui")
+  # candidates AS data only
 west_as_cand <- filter(west_as, scientific_name == "Libytheana carinenta"|
                          scientific_name == "Celastrina echo"|
                          scientific_name == "Gyrocheilus patrobas"|
                          scientific_name == "Eurema mexicana"|
                          scientific_name == "Vanessa cardui")
 
+# Google API Key ----
+key <- read.table("/home/noah/Documents/r-stats/ggmap_API_key")
+register_google(key = key[1,1])
 
-#' visualize iNat data with quick map 
+
+# Quick maps ------------------
 
 
   # Map candidate species west_iNat_cand
@@ -71,6 +84,8 @@ map_cand_as <- get_map(c(left = -125, right = -100, bottom = 28, top = 49)) %>%
              aes(x = longitude, y = latitude, color = scientific_name)) +
   facet_wrap(~scientific_name)
 
+# pre-prep and species_sep for compatibility with Keaton's functions ----
+
 #' Function: prep data for SDM-building function (below)
 #' Split observed_on into date, year
 #' change scientific_name to true_name
@@ -91,18 +106,8 @@ pre_prep <- function(data_in){
 #' into a list of dataframes for each individual species
 #' @param data_in The df to be manipulated
 #' @param tag String to 'tag' onto the end of each auto-generated name in the list  
-    
-species_sep <- function(data_in, tag){
-  data_in$true_name <- droplevels(data_in$true_name)
-  data_in$true_name <- str_replace(data_in$true_name, " ", "_")
-  data_in <- split(data_in, data_in$true_name)
-  new_names <- paste0(names(data_in), tag)
-  glimpse(data_in)
-  names(data_in) <- new_names
-  return(data_in)
-    }
 
-# updated version: leaves true_names intact
+
 species_sep <- function(data_in, tag){
   data_in$true_name <- droplevels(data_in$true_name)
   data_in <- mutate(data_in, list_name = true_name)
@@ -115,7 +120,7 @@ species_sep <- function(data_in, tag){
 }
 
 
-# apply pre_prep and species_sep function 
+# run pre_prep and species_sep on all data ----------
   # AS data
 prepped_as <- pre_prep(west_as_cand)
 prepped_as <- species_sep(prepped_as, "_as")
@@ -266,6 +271,8 @@ G_patrobas_iNat <- prep_data(data = prepped_iNat$Gyrocheilus_patrobas_iNat,
           year_split = 2000, 
           env_raster_t1 = bv_as_t1, env_raster_t2 = bv_as_t2)
 
+saveRDS(G_patrobas_iNat, 'data/G_patrobas_iNat.rds')
+
 # CHECKPOINT
 # get a map of the data to see range size 
 # and determine optimal block size for Block CV function
@@ -343,7 +350,7 @@ run_block_cv = function(prepped_data, bv_raster, block_size = 400000){
   return(blocked)
 }
 
-#' Run run_block_cv function with iNat data -----------------------------------
+# Run run_block_cv iNat -----------------------------------
   # recommended range
 spatialAutoRange(rasterLayer = G_patrobas_iNat$env_data[[2]],
                  sampleNumber = 5000,
@@ -360,6 +367,7 @@ G_patrobas_iNat_blocked <- run_block_cv(prepped_data = G_patrobas_iNat$data[[2]]
              bv_raster = G_patrobas_iNat$env_data[[2]],
              block_size = 25000) #determined by decreasing until all blocks nonzero
 
+saveRDS(G_patrobas_iNat_blocked, "data/G_patrobas_iNat_blocked.rds")
 
 # Preparing data 2 ----------------------------------------------------------
 #' More data preparation prior to SDM building
@@ -384,6 +392,7 @@ prep_data_2 = function(data, env_raster){
 #' Run prep_data2 function with iNat data --------------------------------
 
 G_patrobas_iNat2 <- prep_data_2(G_patrobas_iNat$data[[2]], G_patrobas_iNat$env_data[[2]])
+write.csv(G_patrobas_iNat2, "data/G_patrobas_iNat2.csv")
 
 # Train and test split ---------------------------------------
 
@@ -412,6 +421,8 @@ train_test_split = function(extra_prepped_data, blocked_obj){
 
 #' Run train_test_split function with iNat data -----------------------
 G_patrobas_iNat2_split <- train_test_split(G_patrobas_iNat2, G_patrobas_iNat_blocked)
+
+saveRDS(G_patrobas_iNat2_split, "data/G_patrobas_iNat2_split.rds")
 
 # Modeling ----------------------------------------------------------------
 
@@ -447,7 +458,7 @@ saveRDS(G_patrobas_iNat_model, file = "data/G_patrobas_inat_model.rds")
 
 G_patrobas_iNat_model <- readRDS("data/G_patrobas_inat_model.rds")
 
-# Evaluation plots ---------------------------------------------------------
+  # Evaluation plots ---------------------------------------------------------
 
 eval_plots = function(eval_object = NULL) {
   par(mfrow=c(2,3))
@@ -478,7 +489,7 @@ best_mod = function(model_obj){
 # Run best_mod() on G_patrobas
 
 G_patrobas_iNat_best <- best_mod(G_patrobas_iNat_model)
-
+saveRDS(G_patrobas_iNat_best, "data/G_patrobas_iNat_best.rds")
 
 # Evaluating on test data -------------------------------------------------
 
@@ -495,12 +506,13 @@ evaluate_models = function(test_data, model, env_raster) {
   return(ev)
 }
 
-# run
+# run evaluate_models ----
 
 G_patrobas_iNat_eval <- evaluate_models(test_data = G_patrobas_iNat2_split$test_data, 
                                         model = G_patrobas_iNat_model@models[[14]], 
                                         env_raster = G_patrobas_iNat$env_data[[2]]) 
                                         #index only t2
+saveRDS(G_patrobas_iNat_eval, "data/G_patrobas_iNat_eval.rds")
 
   # Building full models on all data ----------------------------------------
 
@@ -520,15 +532,15 @@ full_model = function(models_obj, best_model_index, full_data = NULL, env_raster
 }
 
 
-# run full_mod for G. patrobas
+# run full_mod iNat -----------
 
 G_patrobas_iNat_full <- full_model(models_obj = G_patrobas_iNat_model, 
                                    best_model_index = G_patrobas_iNat_best[[2]], 
                                    full_data = G_patrobas_iNat_model@occ.pts, 
                                    env_raster = G_patrobas_iNat$env_data[[2]]) #index t2
+saveRDS(G_patrobas_iNat_full, "data/G_patrobas_iNat_full.rds")
 
-
-# testing
+# troubleshooting full_mod ----
 
 auc_mod <- G_patrobas_iNat_model@results[14,]
 FC_best = as.character(auc_mod$features[1])
@@ -541,6 +553,7 @@ full_mod = maxent(G_patrobas_iNat$env_data[[2]],
                   as.matrix(G_patrobas_iNat_model@occ.pts[,1:2]),
                   args = maxent.args[[1]])
 
+
 # Model Plots --------------------------------------------------
 # variable contribution plot
 G_patrobas_iNat_vars <- plot(G_patrobas_iNat_full)
@@ -548,11 +561,31 @@ G_patrobas_iNat_vars <- plot(G_patrobas_iNat_full)
 
 # make predictions using model
 # predict()
-
+pred_Gpa_iNat <- predict(object = G_patrobas_iNat_full,
+                         x = G_patrobas_iNat$env_data[[2]],
+                         ext = G_patrobas_iNat$env_data[[2]]@extent,
+                         args = 'outputformat=cloglog')
 
 # make a spatial pixels dataframe from predictions
-  
+pred_Gpa_iNat <- as(pred_Gpa_iNat, 
+                    "SpatialPixelsDataFrame")
+pred_Gpa_iNat <- as.data.frame(pred_Gpa_iNat) %>%
+  rename("value" = "layer")
 
 # use geom_tile to plot predictions
 # cloglog (P(occ))
 # threshold map 
+
+# basic ggmap
+
+map_Gpa_iNat <- get_map(c(left = -113, right = -107, bottom = 28, top = 36)) %>%
+  ggmap() +
+  geom_tile(data = pred_Gpa_iNat, 
+            aes(x, y, fill=value, alpha = value)) +
+  scale_fill_viridis_c(name = "Probability of Occurence") +
+  guides(alpha = FALSE)
+map_Gpa_iNat
+  # To do: change underlying map to black-and-white
+  #get_map(... color = "bw") has not worked
+
+
